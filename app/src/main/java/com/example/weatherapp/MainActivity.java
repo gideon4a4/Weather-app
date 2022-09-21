9bc7bf46635841bdf24f26e49c6372b155fbcdd2
gkancherla2 <gideon4a4@gmail.com>
2022-09-21 14:47:16 +0530

    slave 1 Commit

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //ahdjhfdjhafj
-        //afiagioh
-                //adghaugh
+        getActionBar().getCustomView().getId();
+
     }
 }
