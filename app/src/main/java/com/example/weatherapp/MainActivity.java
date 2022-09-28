dd0c992e8edfbbe4fb527d6b183956fb6e191b8d
gkancherla2 <gideon4a4@gmail.com>
2022-09-28 15:50:22 +0530

    3

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -9,11 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //ahdjhfdjhafj
-        getActionBar().getCustomView().getId();
-        //fgnjdgjg//
-        //796786786
 
-//scsacasc
+        adgsdhsdfhsdfgjdg
     }
 }
